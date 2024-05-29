e42f693b2aa2cb404758e5f95bd50aed9fcc2192
AnnaRozenbachs <57345254+AnnaRozenbachs@users.noreply.github.com>
2024-05-29 15:48:39 +0200

    Add project files.

---
+++ ProductHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week22
+{
+    public class ProductHelper
+    {
+        public Product SetProductValues(string name, string category,string price,  Product product)
+        {
+            if(string.IsNullOrEmpty(name) || string.IsNullOrEmpty(category) 
+                || string.IsNullOrEmpty(price) || !price.All(char.IsNumber)) {
+                return null; 
+            }
+            product.ProductName = name;
+            product.Category = category;
+            product.Price = decimal.Parse(price);
+            return product;
+        }
+
+        public void DisplayProducts(List<Product> products, string searchedValue= "")
+        {
+            var sortedProducts = products.OrderBy(p => p.Price).ToList();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Name".PadRight(10) + "Category".PadRight(10) + "Price");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (Product product in sortedProducts) 
+            {
+                if (product.ProductName.Contains(searchedValue) || product.Category.Contains(searchedValue))
+                { 
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                Console.WriteLine(product.ProductName.PadRight(10) + product.Category.PadRight(10) + product.Price.ToString().PadRight(10)); 
+                Console.ForegroundColor= ConsoleColor.White;
+            }
+            var sum = sortedProducts.Sum(p => p.Price);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Total: ".PadRight(20) + sum);
+        }
+
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Week22;
+
+List<Product> products = new List<Product>();
+ProductHelper productHelper = new ProductHelper();
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("Enter a product by follow the steps. Search for product in list by entering (s). Quit by enter (q). ");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Enter a productname: ");
+    var name = Console.ReadLine().Trim().ToLower();
+    if (name == "q")
+    {
+        productHelper.DisplayProducts(products);
+    }
+    else if (name == "s")
+    {
+        Console.WriteLine("Enter a search value: ");
+        var searchValue = Console.ReadLine();
+        productHelper.DisplayProducts(products, searchValue);
+    }
+    else
+    {
+        Product product = new Product();
+        Console.WriteLine("Enter a category: ");
+        string category = Console.ReadLine();
+        Console.WriteLine("Enter a price: ");
+        string price = Console.ReadLine();
+        product = productHelper.SetProductValues(name, category, price, product);
+        if (product == null)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Produkten kunde inte läggas till. Något av fälten är ogiltigt ifyllda.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        else
+        {
+            products.Add(product);
+        }
+
+
+    }
+
+}
